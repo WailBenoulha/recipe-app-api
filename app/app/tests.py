1d1644367edeccc97ac0ec8c0eb0b4eda233117b
WailBenoulha <wailbnlh@gmail.com>
2024-07-02 02:16:35 -1100

    version-4

---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase
+from . import calcs
+
+
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        res = calcs.add(5,6)
+        self.assertEqual(res,11)
+    
+    def test_substract(self):
+        res = calcs.substract(10,15)
+        self.assertEqual(res,5)
