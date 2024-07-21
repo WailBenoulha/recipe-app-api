1bc9d12d8eb0cee715fa3f47dd8538d206c2c4ba
WailBenoulha <wailbnlh@gmail.com>
2024-07-21 02:10:12 -1100

    add serializer and view of ingredients

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
-from core.models import Recipe,Tag
+from core.models import Recipe,Tag,Ingredient
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = Ingredient
+        fields = ['id','name']
+        read_only_fields = ['id']
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,10 +15,11 @@ class TagSerializer(serializers.ModelSerializer):
 
 class RecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True,required=False)
+    ingredients = IngredientSerializer(many=True,required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id','title','time_minutes','price','link']
+        fields = ['id','title','time_minutes','price','link','tags','ingredients']
         read_only_fields = ['id']
     
     def get_or_create_tags(self,tags,recipe):
@@ -24,10 +31,23 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def _get_or_create_ingredients(self,ingredients,recipe):
+        auth_user = self.context['request'].user
+        for ingredient in ingredients:
+            ingredient_obj, create = Ingredient.objects.get_or_create(
+                user = auth_user,
+                **ingredient,
+            )
+            recipe.ingredients.add(ingredient_obj)
+
     def create(self,validated_data):
         tags = validated_data.pop('tags',[])
+        ingredients = validated_data.pop('ingredients',[])
         recipe = Recipe.objects.create(**validated_data)
-        auth_user = self.context['request'].user
+        self.get_or_create_tags(tags,recipe)
+        self._get_or_create_ingredients(ingredients,recipe)
+
+        return recipe
         
         return recipe    
     
